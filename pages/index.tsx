9453b2bdbb10c139ccebb7fe4e22bfba78501a2c
Alyona <maksalena04@mail.ru>
2023-06-24 22:38:05 +0300

    Fix text in Login page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Login() {
     <div className={`flex flex-col items-center absolute inset-x-0 inset-y-1/3`}>
       <img className="mb-7 w-5/12" src="./short-logo-t-b.png" />
       <p className="mb-20 text-black text-opacity-90 text-base font-light">
-        Find your best roommates
+        Find your best roommates.
       </p>
       <TelegramButton />
       {/* {JSON.stringify(data)} */}
