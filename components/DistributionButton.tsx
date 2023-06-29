fae34f9c271b137c31403f340328d842023ed8e5
Alyona <maksalena04@mail.ru>
2023-06-29 21:15:00 +0300

    refactor: make button more universal

--- components/DistributionButton.tsx
+++ components/DistributionButton.tsx
@@ -1,9 +1,14 @@
-export default function DistributionButton() {
+interface IDistributionButton {
+  readonly imageSrc: string
+  readonly text: string
+}
+
+export default function DistributionButton({imageSrc, text}: IDistributionButton) {
   return (
     <div className={"btn ml-1"}>
-      <img className="mr-1 w-3" src="./plus.png" />
+      <img className="mr-1 w-3" src={imageSrc} />
       <p className={`text-slate-50 text-sm font-extralight`}>
-        New Distribution
+        {text}
       </p>
     </div>
   );
