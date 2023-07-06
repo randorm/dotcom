60000903218177206ae2ebf101ee9e74a80f2386
Alyona <maksalena04@mail.ru>
2023-07-06 18:14:48 +0300

    style: style: add "y. o." to age of user

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -11,7 +11,7 @@ export default function Bio({ profile }: IBio) {
   return (
     <div className="bioSection">
       <p className="text-lg">{`${profile.firstName} ${profile.lastName}`}</p>
-      <p className="mb-3 opacity-40">{`${profile.gender == "MALE" ? "Male" : "Female"} ${yearOfBirth}`}</p>
+      <p className="mb-3 opacity-40">{`${profile.gender == "MALE" ? "Male" : "Female"} ${yearOfBirth} y. o.`}</p>
       <div className="opacity-90 leading-normal">{profile.bio}</div>
     </div>
   );
