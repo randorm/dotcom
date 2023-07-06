10b1edcf76e09695908ec86f0562ee13b3e430c6
Alyona <maksalena04@mail.ru>
2023-07-06 16:56:44 +0300

    feat: :bug: show gender in lowercase, count age of user from milliseconds

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -4,10 +4,14 @@ export interface IBio {
 }
 
 export default function Bio({ profile }: IBio) {
+  const now = new Date();
+  const birthday = new Date(profile.birthday)
+  const yearOfBirth = now.getFullYear() - birthday.getFullYear()
+
   return (
     <div className="bioSection">
       <p className="text-lg">{`${profile.firstName} ${profile.lastName}`}</p>
-      <p className="mb-3 opacity-40">{`${profile.gender} ${profile.birthday}`}</p>
+      <p className="mb-3 opacity-40">{`${profile.gender == "MALE" ? "Male" : "Female"} ${yearOfBirth}`}</p>
       <div className="opacity-90 leading-normal">{profile.bio}</div>
     </div>
   );
