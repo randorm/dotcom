5b78bf3e3723029a7aa814b4f07075166edeea08
Alyona <maksalena04@mail.ru>
2023-07-16 21:52:42 +0300

    fix: fix image import path

--- components/SelectionButton.tsx
+++ components/SelectionButton.tsx
@@ -1,6 +1,8 @@
 import { MARK_VIEWED, SUBSCRIBE } from "@/graphql/mutations";
 import { useMutation } from "@apollo/client";
 import Image from "next/image";
+import leftArrow from "../public/left-arrow.svg";
+import righrArrow from "../public/right-arrow.svg";
 
 interface ISelectionButton {
   readonly userId: number
@@ -39,10 +41,10 @@ export default function SelectionButton({userId}: ISelectionButton) {
   return (
     <div className="flex w-screen justify-center">
       <div className="bg-black bg-opacity-90 w-3/6 flex justify-center">
-        <Image src="../left-arrow.svg" alt="Dislike the person" />
+        <Image src={leftArrow} alt="Dislike the person" />
       </div>
       <div className="bg-green-600 bg-opacity-90 w-3/6 flex justify-center">
-        <Image src="../right-arrow.svg" alt="Like the person" />
+        <Image src={righrArrow} alt="Like the person" />
       </div>
     </div>
   );
