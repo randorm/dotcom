28102a9e272502ce8f0c5a089676e10c610cb683
Alyona <maksalena04@mail.ru>
2023-07-17 12:34:32 +0300

    fix: feed

--- pages/feed/[id].tsx
+++ pages/feed/[id].tsx
@@ -7,6 +7,8 @@ import { FEED } from "@/graphql/queries";
 import {
   ChoiceAnswer,
   Field,
+  ChoiceField,
+  FieldType,
   Profile,
   TextAnswer,
 } from "@/lib/__codegen__/graphql";
@@ -19,7 +21,7 @@ export default function Feed() {
   const { id } = router.query;
 
   function showNextUser() {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
     if (data.recommend.length > userNumber + 1) {
       setUserNumber(userNumber + 1);
     } else {
@@ -33,15 +35,14 @@ export default function Feed() {
   });
 
   const [profile, setProfile] = useState<Profile>({} as Profile);
-  const [textAnswers, setTextAnswer] = useState<TextAnswer[]>();
-  const [choiceAnswers, setChoiceAnswer] = useState<ChoiceAnswer[]>();
+  const [answers, setAnswers] = useState<(TextAnswer | ChoiceAnswer)[]>();
   const [userNumber, setUserNumber] = useState(0);
   const [userId, setUserId] = useState(0);
 
-  const textAnswersArray: TextAnswer[] = [];
-  const choiceAnswersArray: ChoiceAnswer[] = [];
-  const distributionQuestionsArray: number[] = [];
   useEffect(() => {
+    const answersArray: (TextAnswer | ChoiceAnswer)[] = [];
+    const distributionQuestionsArray: number[] = [];
+
     if (data) {
       console.log(data);
       if (data.recommend.length != 0) {
@@ -56,22 +57,21 @@ export default function Feed() {
               data.recommend[userNumber].answers[i].field.id,
             )
           ) {
-            textAnswersArray.push(data.recommend[userNumber].answers[i]);
+            answersArray.push(data.recommend[userNumber].answers[i]);
           } else if (
             data.recommend[userNumber].answers[i].indices &&
             distributionQuestionsArray.includes(
               data.recommend[userNumber].answers[i].field.id,
             )
           ) {
-            choiceAnswersArray.push(data.recommend[userNumber].answers[i]);
+            answersArray.push(data.recommend[userNumber].answers[i]);
           } else {
             continue;
           }
         }
 
         setProfile(data.recommend[userNumber].profile);
-        setTextAnswer(textAnswersArray);
-        setChoiceAnswer(choiceAnswersArray);
+        setAnswers(answersArray);
         setUserId(data.recommend[userNumber].id);
       }
     }
@@ -87,22 +87,29 @@ export default function Feed() {
             <Bio
               profile={profile}
             />
-            {textAnswers?.map((textAnswer) => (
-              <TextQuestion
-                key={textAnswer.field.id}
-                question={textAnswer.field.question}
-                answer={textAnswer.value}
-              />
-            ))}
-            {choiceAnswers?.map((choiceAnswer) => (
-              <ChoiceQuestion
-                key={choiceAnswer.field.id}
-                options={choiceAnswer.field.options}
-                question={choiceAnswer.field.question}
-                indeces={choiceAnswer.indices}
-              />
-            ))}
-            <button disabled={loading} className="fixed bottom-0" onClick={showNextUser}>
+            {answers?.map((answer) =>
+              answer.type === FieldType.Text
+                ? (
+                  <TextQuestion
+                    key={answer.field.id}
+                    question={answer.field.question}
+                    answer={(answer as TextAnswer).value}
+                  />
+                )
+                : (
+                  <ChoiceQuestion
+                    key={answer.field.id}
+                    options={(answer.field as ChoiceField).options}
+                    question={answer.field.question}
+                    indeces={(answer as ChoiceAnswer).indices}
+                  />
+                )
+            )}
+            <button
+              disabled={loading}
+              className="fixed bottom-0"
+              onClick={showNextUser}
+            >
               <SelectionButton userId={userId} />
             </button>
           </>
