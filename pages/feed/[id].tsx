32b814265f34101873ccb77a8267a65e3a7e1cae
Alyona <maksalena04@mail.ru>
2023-07-22 16:26:42 +0300

    fix: fix

--- pages/feed/[id].tsx
+++ pages/feed/[id].tsx
@@ -79,7 +79,7 @@ export default function Feed() {
         setUserId(data.recommend[userNumber].id);
       }
     }
-  }, [userNumber]);
+  }, [data]);
 
   return (
     <div className="flex flex-col items-center last:mb-10 dark:bg-white">
